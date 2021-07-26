d842f5dfa81ed3562342ec3e5a67695646115153
vish983 <vkkumar983@gmail.com>
2021-07-26 19:30:09 +0530

    UI created and integrated custom search and deletion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'UserCrud';
+export class AppComponent implements OnInit {
+  userArray = [];
+  originalDataArray = [];
+  searchInput = '';
+  constructor() {
+
+  }
+  ngOnInit(): void {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(json => {
+      this.originalDataArray = json;
+      this.userArray = json;
+      console.log(json);
+    });
+  }
+
+  addUser = () => {
+    console.log(this.searchInput);
+  }
+
+  onChangeSearch = (e) => {
+    this.userArray = this.originalDataArray.filter(item => 
+      item.name.toLowerCase().includes(e.toLowerCase()) || item.email.toLowerCase().includes(e.toLowerCase()));
+  }
+
+  deleteUser = (i) => {
+   this.userArray.splice(i, 1);
+  }
 }
